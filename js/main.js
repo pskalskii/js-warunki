cd9ac5e775e02e20608b849f006e621a79592886
pskalskii <pawelskalski97@gmail.com>
2022-04-30 11:53:07 +0200

    js-warunki

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// let number = 8;
+
+// if (number > 10) {
+//   console.log("Podana liczba jest większa od 10");
+// } else if (number > !10) {
+//   console.log("Podana liczba nie jest większa od 10");
+// }
+
+let number = 12;
+
+if (number > 10) {
+  console.log("Podana liczba jest większa od 10");
+} else {
+  console.log("Podana liczba nie jest większa od 10");
+}
